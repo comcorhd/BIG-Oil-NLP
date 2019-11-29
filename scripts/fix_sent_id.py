ae807e2a5429bb3c22bfb55db5d525cf3678e32a
Elvis de Souza <elvis.desouza99@gmail.com>
2019-11-28 23:04:03 -0300

    fix_sent_id com sigla opcional

--- scripts/fix_sent_id.py
+++ scripts/fix_sent_id.py
@@ -2,14 +2,16 @@ import sys
 with open(sys.argv[1]) as f:
     arquivo = f.read().splitlines()
 if len(sys.argv) == 1:
-    print("Entre como parâmetro o arquivo que deseja adicionar # sent_id")
+    print("Entre como parâmetro o arquivo que deseja adicionar # sent_id. É necessário ter # newdoc id.")
+    print("Parâmetro opcional: sigla")
 
 source = ""
 novo_conllu = []
+sigla = "-" + sys.argv[2] if len(sys.argv) > 2 else ""
 for linha in arquivo:
     if linha.startswith("# newdoc id ="):
         source = linha.rsplit("/", 1)[1].rsplit(".txt", 1)[0]
     if not any(linha.startswith(y) for y in ["# newdoc id", "# newpar"]):
-        novo_conllu.append(linha.replace("# sent_id = ", "# sent_id = " + source + "-"))
+        novo_conllu.append(linha.replace("# sent_id = ", "# sent_id = " + source + sigla + "-"))
 
 print("\n".join(novo_conllu))
