2b0611a8567ebc5cb3f3e97ce45597a67b87083e
Elvis de Souza <elvis.desouza99@gmail.com>
2020-07-16 18:20:26 -0300

    update

--- scripts/evaluate_methods.py
+++ scripts/evaluate_methods.py
@@ -5,15 +5,16 @@ import estrutura_ud
 import Cristian_Marneffe
 import validar_UD
 import pprint
+from collections import defaultdict
 import itertools
 
 if len(sys.argv) != 4:
-    print("usage: evaluate_metrics.py system.conllu golden_premodifications.conllu golden_postmodifications.conllu")
+    print("usage: evaluate_methods.py system.conllu golden_premodifications.conllu golden_postmodifications.conllu")
     exit()
 
-sistema = estrutura_ud.Corpus(recursivo=False)
-golden_pre = estrutura_ud.Corpus(recursivo=False)
-golden_post = estrutura_ud.Corpus(recursivo=False)
+sistema = estrutura_ud.Corpus(recursivo=True)
+golden_pre = estrutura_ud.Corpus(recursivo=True)
+golden_post = estrutura_ud.Corpus(recursivo=True)
 sistema.load(sys.argv[1])
 golden_pre.load(sys.argv[2])
 golden_post.load(sys.argv[3])
@@ -32,17 +33,31 @@ erros = validar_UD.validate(golden_pre, errorList = "../ACDC-UD/validar_UD.txt")
 for assunto in erros:
     for error in assunto:
         if error['sentence']:
-            sys.stderr.write(str(error))
             if all(error['sentence'].sent_id in x.sentences for x in [sistema, golden_pre, golden_post]) and all(len(error['sentence'].tokens) == len(x.sentences[error['sentence'].sent_id].tokens) for x in [sistema, golden_pre, golden_post]):
                 if {'sentid': error['sentence'].sent_id, 't': error['t']} not in errors_validar_UD:
                     errors_validar_UD.append({'sentid': error['sentence'].sent_id, 't': error['t']})
 
 confusion_matrix = []
+confusion_matrix_col = defaultdict(list)
+confusion_matrix_hit = defaultdict(lambda: defaultdict(lambda: 0))
+convergence = defaultdict(list)
+convergence_hit = defaultdict(lambda: 0)
 for sentid, sentence in golden_pre.sentences.items():
-    if all(sentid in x.sentences for x in [golden_pre, sistema]) and all(len(sentence.tokens) == len(x.sentences[sentid].tokens) for x in [golden_pre, sistema]):
+    if all(sentid in x.sentences for x in [golden_pre, sistema, golden_post]) and all(len(sentence.tokens) == len(x.sentences[sentid].tokens) for x in [golden_pre, sistema, golden_post]):
         for t, token in enumerate(sentence.tokens):
-            for coluna in ['upos', 'deprel']:
-                if token.col[coluna] != sistema.sentences[sentid].tokens[t].col[coluna]:
+            for coluna in ['upos', 'deprel', 'dephead']:
+                if token.col[coluna] == sistema.sentences[sentid].tokens[t].col[coluna]:
+                    convergence[coluna].append({'sentid': sentid, 't': t})
+                    if token.col[coluna] == golden_post.sentences[sentid].tokens[t].col[coluna]:
+                        convergence_hit[coluna] += 1
+                elif token.col[coluna] != sistema.sentences[sentid].tokens[t].col[coluna]:
+                    confusion_matrix_col[coluna].append({'sentid': sentid, 't': t})
+                    if golden_post.sentences[sentid].tokens[t].col[coluna] == golden_pre.sentences[sentid].tokens[t].col[coluna]:
+                        confusion_matrix_hit[coluna]['golden_pre'] += 1
+                    elif golden_post.sentences[sentid].tokens[t].col[coluna] == sistema.sentences[sentid].tokens[t].col[coluna]:
+                        confusion_matrix_hit[coluna]['sistema'] += 1
+                    else:
+                        confusion_matrix_hit[coluna]['none'] += 1
                     if {'sentid': sentid, 't': t} not in confusion_matrix:
                         confusion_matrix.append({'sentid': sentid, 't': t})
 
@@ -84,10 +99,9 @@ for combination in combinatoria:
         for erro in metodos[metodo]:
             if erro not in somas[combination]:
                 somas[combination].append(erro)
-pprint.pprint(combinatoria)
 
 #COMEÇA A MONTAGEM DOS HTML
-html = "<html><body style='width:40%; margin:auto; margin-top:20px;'>"
+html = "<html><body style='width:40%; padding-bottom:100px; margin:auto; margin-top:20px;'>"
 html += "<h1>Avaliação dos métodos de correção do Julgamento</h1><hr>"
 html += f"<h3>Sistema: {sys.argv[1]}</h3>"
 html += f"<h3>Pré-golden (pré-correções): {sys.argv[2]}</h3>"
@@ -108,19 +122,29 @@ html += "</table><hr>"
 
 html += "<h2>Avaliação dos métodos</h2><hr>"
 html += "<table border='1'>"
-html += "<tr><th>Método</th><th>Erros detectados</th><th title='Erros detectados por todos os métodos'>Erros sobrepostos</th><th>Verdadeiros Positivos</th><th>Falsos Positivos</th></tr>"
+html += "<tr><th>Método</th><th>Erros detectados (por token)</th><th title='Erros detectados por todos os métodos DESSA linha'>Erros repetidos</th><th>Verdadeiros Positivos</th><th>Falsos Positivos</th></tr>"
 
-html += f"<tr><td>Nenhum método</td><td>{len([x for x in all_modifications if not x in [k for metodo in metodos for k in metodos[metodo]]])}</td><td>{len([x for x in all_modifications if not x in [k for metodo in metodos for k in metodos[metodo]]])}</td><td>{len([x for x in all_modifications if not x in [k for metodo in metodos for k in metodos[metodo]]])}</td><td>0</td></tr>"
+html += f"<tr><td>Nenhum método</td><td>{len([x for x in all_modifications if not x in [k for metodo in metodos for k in metodos[metodo]]])}</td><td>Não se aplica</td><td>{len([x for x in all_modifications if not x in [k for metodo in metodos for k in metodos[metodo]]])}</td><td>Não se aplica</td></tr>"
 
 for combination in sorted([x for x in list(somas.keys()) if x], key=lambda x: x[0]):
-    html += f"<tr><td>{' + '.join(combination)}</td><td>{len(somas[combination])}</td><td>{len([x for x in somas[combination] if all(x in somas[(y, )] for y in combination)])}</td><td>{len([x for x in somas[combination] if x in all_modifications])}</td><td>{len([x for x in somas[combination] if x not in all_modifications])}</td></tr>"
+    html += f"<tr><td>{'<b>' if len(combination) == 1 else ''}{' + '.join(combination)}{'</b>' if len(combination) == 1 else ''}</td><td>{len(somas[combination])}</td><td>{len([x for x in somas[combination] if all(x in somas[(y, )] for y in combination)]) if len(combination) > 1 else 'Não se aplica'}</td><td>{len([x for x in somas[combination] if x in all_modifications])}</td><td>{len([x for x in somas[combination] if x not in all_modifications])}</td></tr>"
 
-html += "</table><br><br><br><br>"
+html += "</table><hr>"
 
+html += "<h2>Detalhe: matriz de confusão</h2><hr>"
+html += "<table border='1'>"
+html += "<tr><th></th><th>Divergências (por coluna)</th><th>golden_pre estava correto</th><th>sistema estava correto</th><th>Ninguém estava correto</th><th>Convergências</th><th>Convergência estava correta</th></tr>"
+for coluna in confusion_matrix_col:
+    html += f"<tr><td>{coluna}</td><td>{len(confusion_matrix_col[coluna])}</td><td>{confusion_matrix_hit[coluna]['golden_pre']}</td><td>{confusion_matrix_hit[coluna]['sistema']}</td><td>{confusion_matrix_hit[coluna]['none']}</td><td>{len(convergence[coluna])}</td><td>{convergence_hit[coluna]}</td></tr>"
+html += f"<tr><td>Total</td><td>{sum([len(confusion_matrix_col[coluna]) for coluna in confusion_matrix_col])}</td><td>{sum([confusion_matrix_hit[coluna]['golden_pre'] for coluna in confusion_matrix_col])}</td><td>{sum([confusion_matrix_hit[coluna]['sistema'] for coluna in confusion_matrix_col])}</td><td>{sum([confusion_matrix_hit[coluna]['none'] for coluna in confusion_matrix_col])}</td><td>{sum([len(convergence[coluna]) for coluna in confusion_matrix_col])}</td><td>{sum([convergence_hit[coluna] for coluna in confusion_matrix_col])}</td></tr>"
+html += "</table><hr>"
+
+'''
 for erro in [x for x in errors_validar_UD if x not in all_modifications]:
     html += erro['sentid'] + '-' + golden_post.sentences[erro['sentid']].text
     html += "<br>" + golden_pre.sentences[erro['sentid']].tokens[erro['t']].to_str()
     html += "<br>" + golden_post.sentences[erro['sentid']].tokens[erro['t']].to_str() + "<hr>"
+'''
 
 html += '</body></html>'
 
